119b689e0ccac10213e949ac0f5142ae84687a9f
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-12-07 01:04:16 +0300

    .env

--- disptacher/disptacher/settings.py
+++ disptacher/disptacher/settings.py
@@ -1,14 +1,18 @@
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gk%4y3(*vs_0=)x=+lxsa-f9e_6(hc+_mziq&(av=)%sysanch'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
